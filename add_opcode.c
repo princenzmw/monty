9d70bf78e0ae0c8f9760ac844e0e340cedf6bfc7
princenzmw <princenzmw@gmail.com>
2023-12-15 05:42:58 +0300

    add

--- add_opcode.c
+++ add_opcode.c
@@ -1,5 +1,27 @@
 #include "monty.h"
 
+#include "monty.h"
+
+/**
+  * count_stack - Counts the number of elements in the stack
+  * @stack: The stack to count
+  *
+  * Return: Number of elements in the stack
+  */
+unsigned int count_stack(stack_t *stack)
+{
+	stack_t *current = stack;
+	unsigned int lenght = 0;
+
+	while (current != NULL)
+	{
+		++lenght;
+		current = current->next;
+	}
+
+	return (lenght);
+}
+
 /**
  * add - Adds the top two elements of the stack
  * @stack: The head of the stack
