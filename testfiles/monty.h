b17d52b969ff66cda752bfe4df9f14ac6fdb08fe
princenzmw <princenzmw@gmail.com>
2023-12-17 09:10:55 +0300

    testing my files

--- testfiles/monty.h
+++ testfiles/monty.h
@@ -2,10 +2,12 @@
 #define MONTY_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
+#include <ctype.h>
+
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -18,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -33,8 +35,35 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	 char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#endif /* MONTY_H */
+extern stack_t *head;
+
+void push(stack_t **stack, unsigned int param);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void push_queue(stack_t **stack, unsigned int param);
+void (*pick_func(char *s))(stack_t **, unsigned int);
+void handle_error(int errno, char *opcode, unsigned int line, char *buff);
+void frees_stack(void);
+void check_args_num(int argn);
+FILE *open_file(char *filename);
+int handle_execution(char *op_code, char *op_param, unsigned int line, int m);
+int check_push_param(char *param);
+void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+unsigned int count_stack(stack_t *stack);
+
+#endif
